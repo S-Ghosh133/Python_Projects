764046d0855ac2d4a82621c2759d3a6e52f24aac
D-Ghosh <debopriyog2@gmail.com>
2020-06-14 16:27:37 +0530

    Make some changes

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,13 +1,28 @@
 # Guess the number 
 
-print ('Guess the number if you can !!')
+print("Guess the number if you can !!")
 import random
+
 r = random.randint(1, 9)
-i = eval(input('Enter any number in the range 1 to 9\n'))
+i = int(input("Enter the no. between 0 to 10 that I'm thinking right now\n"))
+a = 10
+b = 0
 if i < r:
-    print ('Greater than this number.')
+    b = i
 elif i > r:
-    print (' Less than this nymber.')
-else :
-    print ('Congratulations!! you win.')
+    a = i
+else:
+    None
+while True:
+    if i < r:
+        i = int(input("Enter a no. greater than %d but less than %d\n" % (i, a)))
+        if i < r:
+            b = i
+    elif i > r:
+        i = int(input("Enter a no. greater than %d but less than %d\n" % (b, i)))
+        if i > r:
+            a = i
+    else:
+        print("Hurray! you guessed the no. which was no. %d\n" % r)
+        break
     
